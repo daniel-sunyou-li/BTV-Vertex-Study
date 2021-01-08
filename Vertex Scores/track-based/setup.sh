84ee934550a9eab92da77da20cf2106e42dc2214
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2021-01-08 16:33:26 -0500

    Update setup.sh

--- Vertex Scores/track-based/setup.sh
+++ Vertex Scores/track-based/setup.sh
@@ -1,32 +1,32 @@
 # copy the analysis scripts to the /test/ directory
 echo Copying analysis script study_pv_sorting.py to CMSSW_10_6_16/src/CommonTools/RecoAlgos/test/
-cp study_pv_sorting.py ../../
+cp study_pv_sorting.py ../../../
 
 # replace the pvSorting.py script
 echo Updating CMSSW_10_6_16/src/CommonTools/RecoAlgos/test/pvSorting.py
-rm ../../pvSorting.py
-cp pvSorting.py ../../
+rm ../../../pvSorting.py
+cp pvSorting.py ../../../
 
 # replace the PrimaryVertexSorter.h script
 echo Updating CMSSW_10_6_16/src/CommonTools/RecoAlgos/plugins/PrimaryVertexSorter.h
-rm ../../../plugins/PrimaryVertexSorter.h
-cp PrimaryVertexSorter.h ../../../plugins/
+rm ../../../../plugins/PrimaryVertexSorter.h
+cp PrimaryVertexSorter.h ../../../../plugins/
 
 # replace the sortedPrimaryVertices_cfi.py script
 echo Updating CMSSW_10_6_16/src/CommonTools/RecoAlgos/python/sortedPrimaryVertices_cfi.py
-rm ../../../python/sortedPrimaryVertices_cfi.py
-cp sortedPrimaryVertices_cfi.py ../../../python/
+rm ../../../../python/sortedPrimaryVertices_cfi.py
+cp sortedPrimaryVertices_cfi.py ../../../../python/
 
 # replace the PrimaryVertexSorting.cc script
 echo Updating CMSSW_10_6_16/src/CommonTools/RecoAlgos/src/PrimaryVertexSorting.cc
-rm ../../../src/PrimaryVertexSorting.cc
-cp PrimaryVertexSorting.cc ../../../src/
+rm ../../../../src/PrimaryVertexSorting.cc
+cp PrimaryVertexSorting.cc ../../../../src/
 
 # replace the PrimaryVertexSorting.h script
 echo Updating CMSSW_10_6_16/src/CommonTools/RecoAlgos/interface/PrimaryVertexSorting.h
-rm ../../../interface/PrimaryVertexSorting.h
-cp PrimaryVertexSorting.h ../../../interface/
+rm ../../../../interface/PrimaryVertexSorting.h
+cp PrimaryVertexSorting.h ../../../../interface/
 
 echo Recompiling the Primary Vertex Sorting scripts
-cd ../../../
+cd ../../../../
 scram b -j4
