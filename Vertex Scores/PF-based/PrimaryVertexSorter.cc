c7a53d5a418972aa01af9d5e1f3aa1c50711d125
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2021-01-24 16:10:47 -0500

    Create PrimaryVertexSorter.cc

---
+++ Vertex Scores/PF-based/PrimaryVertexSorter.cc
@@ -0,0 +1,9 @@
+#include "CommonTools/RecoAlgos/plugins/PrimaryVertexSorter.h"
+#include "DataFormats/RecoCandidate/interface/RecoChargedRefCandidate.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+
+typedef  PrimaryVertexSorter<std::vector<reco::RecoChargedRefCandidate> > RecoChargedRefCandidatePrimaryVertexSorter ;
+DEFINE_FWK_MODULE(RecoChargedRefCandidatePrimaryVertexSorter);
+typedef  PrimaryVertexSorter<std::vector<reco::PFCandidate> > PFCandidatePrimaryVertexSorter;
+DEFINE_FWK_MODULE(PFCandidatePrimaryVertexSorter);
