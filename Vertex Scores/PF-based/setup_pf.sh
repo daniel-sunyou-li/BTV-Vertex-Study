8a0080993d3b1a1302b0c6b5c823907c844fef8f
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2021-01-24 17:38:59 -0500

    Update setup_pf.sh

--- Vertex Scores/PF-based/setup_pf.sh
+++ Vertex Scores/PF-based/setup_pf.sh
@@ -1,26 +1,37 @@
+#!/bin/sh
+
+useTiming=${1} # 3D or 4D
+
+if [ $useTiming = "3D" ]
+then
+  echo Setting up workspace with track assignment not using timing
+  echo Updating CMSSW_10_6_16/src/CommonTools/RecoAlgos/plugins/PrimaryVertexSorter.h
+  cp 3D/PrimaryVertexSorter.h ../../../../plugins/
+  echo Updating CMSSW_10_6_16/src/CommonTools/RecoAlgos/python/sortedPFPrimaryVertices_cfi.py
+  cp 3D/sortedPFPrimaryVertices_cfi.py ../../../../python/
+fi
+if [ $useTiming = "4D" ]
+then
+  echo Setting up workspace with track assignment using timing
+  echo Updating CMSSW_10_6_16/src/CommonTools/RecoAlgos/plugins/PrimaryVertexSorter.h
+  cp 4D/PrimaryVertexSorter.h ../../../../plugins/
+  echo Updating CMSSW_10_6_16/src/CommonTools/RecoAlgos/python/sortedPFPrimaryVertices_cfi.py
+  cp 4D/sortedPFPrimaryVertices_cfi.py ../../../../python
+fi
+
 # copy the analysis scripts to the /test/ directory
 echo Copying analysis script study_pv_sorting.py to CMSSW_10_6_16/src/CommonTools/RecoAlgos/test/
 cp study_pf_sorting.py ../../../
 cp pf_contents.py ../../../
 
 # replace the pvSorting.py script
-echo Updating CMSSW_10_6_16/src/CommonTools/RecoAlgos/test/pvSorting.py
+echo Adding new CMSSW_10_6_16/src/CommonTools/RecoAlgos/test/pvSorting.py script: pvSorting_pf.py
 cp pvSorting_pf.py ../../../
-
-# replace the PrimaryVertexSorter.h script
-echo Updating CMSSW_10_6_16/src/CommonTools/RecoAlgos/plugins/PrimaryVertexSorter.h
-rm ../../../../plugins/PrimaryVertexSorter.h
-cp PrimaryVertexSorter.h ../../../../plugins/
 
 # replace the PrimaryVertexSorter.cc script
 echo Updating CMSSW_10_6_16/src/CommonTools/RecoAlgos/plugins/PrimaryVertexSorter.cc
 rm ../../../../plugins/PrimaryVertexSorter.cc
 cp PrimaryVertexSorter.cc ../../../../plugins/
-
-# replace the sortedPrimaryVertices_cfi.py script
-echo Updating CMSSW_10_6_16/src/CommonTools/RecoAlgos/python/sortedPrimaryVertices_cfi.py
-rm ../../../../python/sortedPrimaryVertices_cfi.py
-cp sortedPrimaryVertices_cfi.py ../../../../python/
 
 # replace the PrimaryVertexSorting.cc script
 echo Updating CMSSW_10_6_16/src/CommonTools/RecoAlgos/src/PrimaryVertexSorting.cc
